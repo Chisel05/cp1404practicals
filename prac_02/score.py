03471f7df80c01234a801c0a0f3a392f857d3713
Dez <malisgotit@yahoo.com>
2023-09-24 20:27:02 +1000

    Add function for determining score result, and add random score and its result

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,16 +1,30 @@
 """
 CP1404/CP5632 - Practical
-Broken program to determine score status
+Program to determine score result
 """
 
-# TODO: Fix this!
+from random import randint
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:  # score < 50
-    print("Bad")
+
+def main():
+    """Determine and print score result with a random score result"""
+    score = float(input("Enter score: "))
+    random_score = randint(0, 100)
+
+    print(f"Your score '{score}' is: {determine_score_result(score)}\n"
+          f"Random score '{random_score}' is: {determine_score_result(random_score)}")
+
+
+def determine_score_result(score):
+    """Determine score result from provided value"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:  # score < 50
+        return "Bad"
+
+
+main()
