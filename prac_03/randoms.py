7102955dd2ecf8c5fbd03425f1460f714bf9690f
Dez <malisgotit@yahoo.com>
2023-10-01 19:29:09 +1000

    Add randoms.py prac questions' answers

---
+++ prac_03/randoms.py
@@ -0,0 +1,17 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+# 13
+# 5, 20
+
+# 5
+# 3, 9
+# no
+
+# 3.943529300916592
+# 2.5, 5.5
+
+print(random.randint(1, 100))
