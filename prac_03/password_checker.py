30314d63ff401da8297a75ec45cde1940255dc68
Dez <malisgotit@yahoo.com>
2023-10-01 21:50:08 +1000

    Add password_checker.py TO-DO fixes

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -3,8 +3,8 @@ CP1404/CP5632 - Practical
 Password checker "skeleton" code to help you get started
 """
 
-MIN_LENGTH = 2
-MAX_LENGTH = 6
+MIN_LENGTH = 5
+MAX_LENGTH = 15
 SPECIAL_CHARS_REQUIRED = False
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
@@ -16,7 +16,8 @@ def main():
           "characters, and contain:")
     print("\t1 or more uppercase characters")
     print("\t1 or more lowercase characters")
-    print("\t1 or more numbers")
+    print("\t1 or more numbers"
+          "\tand 1 or more special characters:", SPECIAL_CHARACTERS)
     if SPECIAL_CHARS_REQUIRED:
         print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
     password = input("> ")
@@ -28,23 +29,27 @@ def main():
 
 def is_valid_password(password):
     """Determine if the provided password is valid."""
-    # TODO: if length is wrong, return False
+    password_length = len(password)
+    if password_length < MIN_LENGTH or password_length > MAX_LENGTH:
+        return False
 
     count_lower = 0
     count_upper = 0
     count_digit = 0
     count_special = 0
     for char in password:
-        # TODO: count each kind of character (use str methods like isdigit)
-        pass
-
-    # TODO: if any of the 'normal' counts are zero, return False
-
-    # TODO: if special characters are required, then check the count of those
-    # and return False if it's zero
-
-    # if we get here (without returning False), then the password must be valid
-    return True
+        if char.isdigit():
+            count_digit += 1
+        if char.islower():
+            count_lower += 1
+        if char.isupper():
+            count_upper += 1
+        for special_character in SPECIAL_CHARACTERS:
+            if special_character == char:
+                count_special += 1
+    if count_lower == 0 or count_upper == 0 or count_digit == 0 or count_special == 0:
+        return False
+    return True  # password must be valid for True
 
 
 main()
