00429b117f266cbbafac0454da82b042f2ae70c2
Dez <malisgotit@yahoo.com>
2023-10-01 22:13:18 +1000

    Reformat print functions with f-strings & fix SPECIAL_CHARS_REQUIRED

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -5,21 +5,20 @@ Password checker "skeleton" code to help you get started
 
 MIN_LENGTH = 5
 MAX_LENGTH = 15
-SPECIAL_CHARS_REQUIRED = False
+SPECIAL_CHARS_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
 def main():
     """Program to get and check a user's password."""
-    print("Please enter a valid password")
-    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
-          "characters, and contain:")
-    print("\t1 or more uppercase characters")
-    print("\t1 or more lowercase characters")
-    print("\t1 or more numbers"
-          "\tand 1 or more special characters:", SPECIAL_CHARACTERS)
+    print(f"Please enter a valid password"
+          f"\nYour password must be between {MIN_LENGTH} and {MAX_LENGTH}, and contain:"
+          f"\n\t1 or more uppercase characters"
+          f"\n\t1 or more lowercase characters"
+          f"\n\t1 or more numbers")
     if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+        print(f"\tand 1 or more special characters: {SPECIAL_CHARACTERS}")
+
     password = input("> ")
     while not is_valid_password(password):
         print("Invalid password!")
@@ -47,8 +46,11 @@ def is_valid_password(password):
         for special_character in SPECIAL_CHARACTERS:
             if special_character == char:
                 count_special += 1
-    if count_lower == 0 or count_upper == 0 or count_digit == 0 or count_special == 0:
+    if count_lower == 0 or count_upper == 0 or count_digit == 0:
         return False
+    if SPECIAL_CHARS_REQUIRED:
+        if count_special == 0:
+            return False
     return True  # password must be valid for True
 
 
