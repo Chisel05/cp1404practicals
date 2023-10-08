2e535541c75706ee25fc7939beeb1f782848ee08
Chisel05 <chisel.webber@my.jcu.edu.au>
2023-10-08 21:59:54 +1000

    Add username security check

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -5,14 +5,23 @@ Prac 4, list_exercises.py
 
 def main():
     numbers = []
-    for i in range(5):
-        number = int(input("Number: "))
-        numbers.append(number)
-    print(f"The first number is {numbers[0]}"
-          f"\nThe last number is {numbers[-1]}"
-          f"\nThe smallest number is {min(numbers)}"
-          f"\nThe largest number is {max(numbers)}"
-          f"\nThe average of the numbers is {sum(numbers) / len(numbers)}")
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    username = input("Username: ")
+
+    if username in usernames:
+        print("Access granted\n")
+        for i in range(5):
+            number = int(input("Number: "))
+            numbers.append(number)
+        print(f"The first number is {numbers[0]}"
+              f"\nThe last number is {numbers[-1]}"
+              f"\nThe smallest number is {min(numbers)}"
+              f"\nThe largest number is {max(numbers)}"
+              f"\nThe average of the numbers is {sum(numbers) / len(numbers)}")
+    if username not in usernames:
+        print("Access denied")
 
 
 main()
