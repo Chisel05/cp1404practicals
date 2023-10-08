2b11c3a177a97b039bd693f917f583704d93e144
Chisel05 <chisel.webber@my.jcu.edu.au>
2023-10-08 21:02:34 +1000

    Add lists_warmup.py

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,25 @@
+"""
+Lists_warmup.py
+"""
+
+
+def main():
+    numbers = ["ten", 1, 4, 1, 5, 9, 1]
+    print(numbers[2:])
+    print(f"9 in numbers? {9 in numbers}")
+
+
+"""
+3
+2
+1
+3, 1, 4, 1, 5, 9
+1
+True
+False
+False
+[3, 1, 4, 1, 5, 9, 2]
+"""
+
+
+main()
