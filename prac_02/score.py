1eba4ab3b64dc5b844958d51b742eb577a0e6b62
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-09-29 21:51:07 +1000

    Reformat score program into functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,13 +3,22 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score > 100 or score < 0:
-    score_category = "Invalid score"
-elif score >= 90:
-    score_category = "Excellent"
-elif score >= 50:
-    score_category = "Passable"
-else:
-    score_category = "Bad"
-print(score_category)
+
+def main():
+    score = float(input("Enter score: "))
+    print(get_score_category(score))
+
+
+def get_score_category(score):
+    if score > 100 or score < 0:
+        score_category = "Invalid score"
+    elif score >= 90:
+        score_category = "Excellent"
+    elif score >= 50:
+        score_category = "Passable"
+    else:
+        score_category = "Bad"
+    return score_category
+
+
+main()
