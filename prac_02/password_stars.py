5cba9c2a246197f05561e8c813738921fef14dd6
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-09-29 21:24:55 +1000

    Add main function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,9 +2,14 @@
 Program that prints asterisks the length of a valid, user provided password.
 """
 
-minimum_length = 5
-password = input("Password: ")
-while len(password) < minimum_length:
-    print(f"Must be >= {minimum_length}!")
+
+def main():
+    minimum_length = 5
     password = input("Password: ")
-print('*' * len(password))
+    while len(password) < minimum_length:
+        print(f"Must be >= {minimum_length}!")
+        password = input("Password: ")
+    print('*' * len(password))
+
+
+main()
