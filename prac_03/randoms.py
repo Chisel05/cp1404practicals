05e55bddf932a8f9126939c9896c07df303a6bc5
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-10-06 13:14:42 +1000

    Add code & comments with answers

---
+++ prac_03/randoms.py
@@ -0,0 +1,6 @@
+import random
+
+print(random.randint(5, 20))  # Smallest number could have been 5. Largest could have been 20.
+print(random.randrange(3, 10, 2))  # Smallest = 3. Largest = 9. Couldn't have produced a 4.
+print(random.uniform(2.5, 5.5))  # Smallest number could have been 2.5, largest could have been 5.5.
+print(random.randrange(1, 101))
