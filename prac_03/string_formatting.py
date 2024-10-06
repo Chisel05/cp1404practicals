53229d524434fbfd6254a2a4db53f9b04a3f7c6c
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-10-06 12:24:32 +1000

    Add solution to second TODO

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -32,19 +32,9 @@ numbers = [1, 19, 123, 456, -25]
 for i, number in enumerate(numbers, 1):
     print(f"Number {i} is {number:5}")
 
-# Solution to first TODO:
+# First solution:
 print(f"{year} {name} for about {cost:,.0f}!")
 
-# TODO: Using a for loop with the range function and f-string formatting,
-# produce the following right-aligned output (DO NOT use a list):
-# 2 ^ 0 is    1
-# 2 ^ 1 is    2
-# 2 ^ 2 is    4
-# 2 ^ 3 is    8
-# 2 ^ 4 is   16
-# 2 ^ 5 is   32
-# 2 ^ 6 is   64
-# 2 ^ 7 is  128
-# 2 ^ 8 is  256
-# 2 ^ 9 is  512
-# 2 ^10 is 1024
+# Second solution:
+for i in range(0, 11):
+    print(f"{2} ^{i:>2} is {2 ** i:>4}")
