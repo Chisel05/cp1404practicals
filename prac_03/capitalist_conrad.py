9a34ab36871fb2c422aa0103677eeb4fd4286c2e
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-10-06 13:36:45 +1000

    Add modified range, max increase & storage of simulation results to file

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,16 +9,20 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1
+MAX_PRICE = 100
 INITIAL_PRICE = 10.0
+FILENAME = "capitalist_conrad_log.txt"
 
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
+number_of_days = 0
+out_file = open(FILENAME, 'w')
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
+    number_of_days = number_of_days + 1
     price_change = 0
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
@@ -32,4 +36,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {number_of_days} price is ${price:,.2f}", file=out_file)
+out_file.close()
