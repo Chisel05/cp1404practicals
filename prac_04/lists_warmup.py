8f42a4eaaf0d47fe29643893359d62656bcf0661
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-10-13 10:36:53 +1000

    Add lists_warmup.py with answers to questions

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,28 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Q: numbers[0]
+# A: 3
+
+# Q: numbers[-1]
+# A: 2
+
+# Q: numbers[3]
+# A: 1
+
+# Q: numbers[:-1]
+# A: [3, 1, 4, 1, 5, 9]
+
+# Q: numbers[3:4]
+# A: [1]
+
+# Q: 5 in numbers
+# A: True
+
+# Q: 7 in numbers
+# A: False
+
+# Q: "3" in numbers
+# A: False
+
+# Q: numbers + [6, 5, 3]
+# A: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
