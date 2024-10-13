e11ece7cc1873ea9c81dacb5d80f51c4a7d161db
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-10-13 12:06:18 +1000

    Add 'print_report()' function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,6 +13,10 @@ def main():
         income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
 
+    print_report(incomes, number_of_months)
+
+
+def print_report(incomes, number_of_months):
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
