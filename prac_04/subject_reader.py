03da1586d04e36369faa329771a131b4764a7ce6
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-10-13 12:41:42 +1000

    Add function that displays subjects & code that processes lines into lists

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -10,20 +10,25 @@ def main():
     data = load_data()
     print(data)
 
+    display_subjects(data)
+
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line_parts = line.strip().split(',')
+        line_parts[2] = int(line_parts[2])
+        data.append(line_parts)
     input_file.close()
+    return data
+
+
+def display_subjects(data):
+    for subject in range(len(data)):
+        subject_parts = data[subject]
+        print(f"{subject_parts[0]} is taught by {subject_parts[1]} and has {subject_parts[2]}")
 
 
 main()
