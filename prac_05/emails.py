a1ca5f4dd14be95561cac7ec26153cc013b93eb1
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-10-19 16:43:02 +1000

    Add working code & actual time

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,5 +1,22 @@
 """
 Word Occurrences
 Estimate: 15 minutes
-Actual:
+Actual:   25 minutes
 """
+
+emails = []
+name_to_email = {}
+email = input("Email: ")
+while email != "":
+    emails.append(email)
+    name = email.split("@")[0].split(".")
+    name = " ".join(name).title()
+    name_confirmation = input(f"Is your name {name}? (Y/n) ").lower()
+    if name_confirmation == "n":
+        name = input("Name: ").title()
+    name_to_email[name] = email
+    email = input("Email: ")
+
+print()
+for name in name_to_email:
+    print(f"{name} ({name_to_email[name]})")
