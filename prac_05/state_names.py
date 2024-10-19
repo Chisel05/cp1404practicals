a95930ffc70c78ad504b013bf28ee54ea5089f50
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-10-19 13:58:45 +1000

    Swap if-else to try-except

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,9 +9,9 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
 print(CODE_TO_NAME)
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
 
