0bcea4e06645ed6bd5e6a175f0bd97c40f9640ce
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-10-19 13:54:05 +1000

    Add loop that prints formatted state codes & state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -3,12 +3,10 @@ CP1404/CP5632 Practical
 State names in a dictionary
 File needs reformatting
 """
-
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
 
+print(CODE_TO_NAME)
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
@@ -16,3 +14,7 @@ while state_code != "":
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
+
+for state_code in CODE_TO_NAME:  # 'state_code' variable repurposed as iteration variable for loop & no longer used for user input
+    print(
+        f"{state_code:3} is {CODE_TO_NAME[state_code]:28}")  # Chose not to use dynamic spacing, since it's unlikely that new states will be added or exiting states removed
