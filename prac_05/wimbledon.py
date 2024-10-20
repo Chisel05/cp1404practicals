14d165e4df905a843deaa5a8b3be5240aa146239
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-10-20 16:59:53 +1000

    Refactor program into 3 separate functions (for reading file, processing file, and printing processed data), cut down use of sequences to one list of lists, one set, and one dictionary, and add comments where needed

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,55 +1,43 @@
 """
-REQUIREMENTS:
-list of lists = 1
-sets = 1
-dictionaries = 1
-functions = 4 (including main)
-& final output done using '.join()' string method
-
-WHAT I HAVE:
-lists = 1, records_data
-sets = 2, names & countries (ONE OVER!)
-dictionaries = 1, name_to_championships_won
-functions = 2, main() & get_records_data()
-& final output uses '.join()' string method
-
-TO BE DONE:
-- eliminate (1) set without sacrificing functionality
-- refactor main into (2) extra functions
+Program that reads the records of 'Wimbledon gentlemen's singles champions' as CSV, processes those records, and prints
+how many times each champion has won & a list of countries that have won the competition in alphabetical order.
 """
 FILENAME = 'wimbledon.csv'
 
 
 def main():
-    records_data = get_records_data(FILENAME)
-
-    names = {line_data[2] for line_data in records_data[1:]}
-
-    name_to_championships_won = {}
-    for name in names:
-        for line_data in records_data:
-            if line_data[2] == name:
-                if name in name_to_championships_won:
-                    name_to_championships_won[name] += 1
-                else:
-                    name_to_championships_won[name] = 1
-
+    records = get_records(FILENAME)[1:]  # '[1:]' slices first processed (example) line before storing in 'records'
+    name_to_championships_won = get_name_to_championships_won(records)
     print("Wimbledon Champions:")
-    for name in name_to_championships_won:
-        print(f"{name} {name_to_championships_won[name]}")
+    print_key_and_value(name_to_championships_won)
 
-    countries = {line_data[3] for line_data in records_data[1:]}
-    countries = sorted([country for country in countries])
+    countries = {record[3] for record in records}
     print(f"\nThese {len(countries)} countries have won Wimbledon:")
-    print(", ".join(countries))
+    print(", ".join(sorted(countries)))
 
 
-def get_records_data(filename):
-    records_data = []
+def get_records(filename):
+    """Returns list of records from CSV file."""
     with open(filename, "r", encoding="utf-8-sig") as in_file:
-        for line in in_file:
-            records_data.append(line.strip().split(","))
-    return records_data
+        records = [line.strip().split(",") for line in in_file]
+    return records
+
+
+def get_name_to_championships_won(records):
+    """Iterates through record of records, adding each new name as a new key of dictionary, or incrementing key's value by 1 (if the name, as 'record[2]', is an existing key)"""
+    name_to_championships_won = {}
+    for record in records:
+        if record[2] in name_to_championships_won:
+            name_to_championships_won[record[2]] += 1  # Increment existing 'record[2]' key
+        else:
+            name_to_championships_won[record[2]] = 1  # New name 'record[2]' key, with value of 1
+    return name_to_championships_won
+
+
+def print_key_and_value(key_to_value):
+    """Function that prints each key & value of that key from given dictionary"""
+    for key in key_to_value:
+        print(f"{key} {key_to_value[key]}")
 
 
 main()
