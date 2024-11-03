3a8d15202bc19fa8edb43733eb5d9bc124ccf419
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-11-03 15:23:39 +1000

    Complete guitars.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,4 +1,28 @@
 """
 Estimate time: 25min
-Actual time:
+Actual time: 35min
 """
+from guitar import Guitar
+
+
+def main():
+    guitars = []
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost:,.2f} added.")
+        name = input("Name: ")
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i + 1}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+main()
