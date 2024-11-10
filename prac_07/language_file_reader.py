6073c0e331260c1d3c15b8f6c91a2d592dfd1450
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-11-10 14:03:17 +1000

    Add modifications to language_file_reader.py, programming_language.py, & languages.csv

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -26,9 +26,11 @@ def main():
         # print(parts)  # debugging
         # Reflection is stored as a string (Yes/No) and we want a Boolean
         reflection = parts[2] == "Yes"
+        # Pointer Arithmetic is stored as a string (Yes/No) and we want a Boolean
+        pointer_arithmetic = parts[4] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), pointer_arithmetic)
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,16 +7,17 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, Pointer Arithmetic={self.pointer_arithmetic}"
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
