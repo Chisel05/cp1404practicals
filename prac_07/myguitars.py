30b6e42f7da1b3ec99347e69dc7977ff6113dbef
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-11-10 14:23:39 +1000

    Add code that opens the CSV file, stores each line as an object within a list, and displays the objects of that list

---
+++ prac_07/myguitars.py
@@ -0,0 +1,19 @@
+"""
+Program that reads all guitar entries from a CSV file and stores them in a list of Guitar objects.
+"""
+from guitar import Guitar
+
+
+def main():
+    """Function reads CSV file, adding each entry as an object within a list"""
+    with open('guitars.csv', 'r') as in_file:
+        guitars = []
+        for line in in_file:
+            parts = line.strip().split(',')
+            guitars.append(Guitar(parts[0], parts[1], float(parts[2])))
+
+    for guitar in guitars:
+        print(guitar)
+
+
+main()
