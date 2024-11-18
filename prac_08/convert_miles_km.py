a93037cfefdcd061cf2c21bb7e338cad06a4e54e
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-11-18 21:01:05 +1000

    Add basic app code to convert_miles_km.py and layout to convert_miles_km.kv

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,16 @@
+"""
+'Do-from-scratch' exercise from the week 8 practical.
+"""
+from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.lang import Builder
+
+
+class MilesKmApp(App):
+    def build(self):
+        self.title = 'Miles and Km'
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+
+MilesAndKmApp().run()
