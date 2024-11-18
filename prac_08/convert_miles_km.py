2f43e9bf6ae9929b8287c55c26b86bf9ef0990ca
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-11-18 21:37:03 +1000

    Complete from scratch exercise from practical 8 (up to stage 2)

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -2,15 +2,34 @@
 'Do-from-scratch' exercise from the week 8 practical.
 """
 from kivy.app import App
-from kivy.properties import StringProperty
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILES_TO_KM = 1.60934
 
 
 class MilesKmApp(App):
+    km = StringProperty()
+
     def build(self):
         self.title = 'Miles and Km'
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_conversion(self):
+        try:
+            miles = float(self.root.ids.miles.text)
+            self.km = str(miles * MILES_TO_KM)
+        except ValueError:
+            self.km = str(0.0)
+
+    def handle_increment(self, increment):
+        try:
+            miles = int(self.root.ids.miles.text) + increment
+            self.root.ids.miles.text = str(miles)
+        except ValueError:
+            miles = 0 + increment
+            self.root.ids.miles.text = str(miles)
 
 
 MilesKmApp().run()
