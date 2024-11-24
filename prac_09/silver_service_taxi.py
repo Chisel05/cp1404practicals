f29a01366880acbee948985216701383b9952fb9
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-11-24 15:36:41 +1000

    Complete silver_service_taxi.py & silver_service_taxi_test.py according to exercise

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,22 @@
+"""
+Module containing 'SilverServiceTaxi' class.
+"""
+from taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """SilverServiceTaxi class."""
+    flagfall = 4.50
+
+    def __init__(self, fanciness: float, **kwargs):
+        """Define SilverServiceTaxi class."""
+        super().__init__(**kwargs)
+        self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * self.fanciness
+
+    def __str__(self):
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Return the price for the silver service taxi trip."""
+        return self.price_per_km * self.current_fare_distance + self.flagfall
