999761eb57f05f82e05dbfa694249280948d6620
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-11-24 15:58:30 +1000

    Add docstring to str method of silver_service_taxi.py

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -15,6 +15,7 @@ class SilverServiceTaxi(Taxi):
         self.price_per_km = Taxi.price_per_km * self.fanciness
 
     def __str__(self):
+        """Define string method."""
         return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
 
     def get_fare(self):
