ea44590b7b1310307563394f5ba2b6735e7daadd
Chisel05 <chisel.webber@my.jcu.edu.au>
2024-11-30 21:03:34 +1000

    Complete testing.py

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,7 +4,7 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, name, fuel=0):
+    def __init__(self, name='', fuel=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
